2c88e289d8832e0f1c5cafa9852d9c254435cce3
SaihajMann <saihajmann07@gmail.com>
2025-04-08 21:06:06 -0600

    better rotation

--- src/app/grade9/rotations/page.js
+++ src/app/grade9/rotations/page.js
@@ -1,25 +1,28 @@
 'use client'
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [coorindates, setCoorindates] = useState("")
   const [xCoordinate, setXCoordinate] = useState(0)
   const [yCoordinate, setYCoordinate] = useState(0)
 
   const [newXCoordinate, setNewXCoordinate] = useState(0)
   const [newYCoordinate, setNewYCoordinate] = useState(0)
 
+  useEffect(() => {
+    setNewXCoordinate(0-yCoordinate)
+    setNewYCoordinate(xCoordinate)
+
+  },[xCoordinate, yCoordinate])
+
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      Math made fun
-
+    <div className="grid items-center justify-items-center">
       Rotations
 
-      take point and rotate 90 degrees
-      <input type="number" value={xCoordinate} onChange={(e) => {setXCoordinate(e.target.value)}}/>
-      <input type="number" value={yCoordinate} onChange={setYCoordinate}/>
+      Take a point and rotate 90 degrees
+      <span>X <input type="number" value={xCoordinate} onChange={(e) => {setXCoordinate(e.target.value)}}/></span>
+      <span>Y <input type="number" value={yCoordinate} onChange={(e) => {setYCoordinate(e.target.value)}}/></span>
 
       {newXCoordinate}, {newYCoordinate}
     </div>
