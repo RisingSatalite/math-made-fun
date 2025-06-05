b8b3aa048bdd7d5644102bcb8544859770ce9d76
SaihajMann <saihajmann07@gmail.com>
2025-06-04 22:33:00 -0600

    surafece area cube

--- src/app/grade9/surfacearea/page.js
+++ src/app/grade9/surfacearea/page.js
@@ -1,12 +1,14 @@
 'use client'
 
 import { useState } from "react";
+import SurfaceAreaCube from "@/components/surfaceAreaCube";
 
 export default function Home() {
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
       <p>Calculate surface area of shapes</p>
+      <SurfaceAreaCube/>
       <p>L * W = A</p>
       <p>2 * (L * W + L * H + W * H) = SA of a cube</p>
       <p>2 * (L * W + L * H + W * H) = SA of a cuboid</p>
