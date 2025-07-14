c7cc28aad88a2e24323ba3c0ac1cb3d4a6cd887c
SaihajMann <saihajmann07@gmail.com>
2025-07-14 14:28:22 -0600

    links

--- src/app/theories/page.js
+++ src/app/theories/page.js
@@ -2,7 +2,10 @@ export default async function Theories() {
   return (
     <div>
       <h1>Mathematics theories</h1>
-      Math has many theories
+      <p>Math has many theories</p>
+      <p>Set Thoery, about sets or groups of objects: 
+        <a href={`/theories/set-theory`}>{`Set Theory Page`}</a>
+      </p>
     </div>
   );
 }
