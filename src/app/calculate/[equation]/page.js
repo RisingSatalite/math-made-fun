4bd55c77238cb331c30b7927520745ea67c54185
RisingSatalite <saihajmann07@gmail.com>
2025-08-10 21:52:54 -0600

    dynamic equation solver

---
+++ src/app/calculate/[equation]/page.js
@@ -0,0 +1,17 @@
+'use client'
+
+export default function Page({ params }) {
+  // params.id comes directly from the URL
+
+  // const cannot be used since nerdamer gets modified when other modules are loaded  
+  var nerdamer = require('nerdamer'); 
+  // Load additional modules. These are not required.  
+
+  var solution = nerdamer(params.equation);
+  return (
+    <div>
+      <h1>You entered: {params.equation}</h1>
+      <p>{solution.text()}</p>
+    </div>
+  );
+}
