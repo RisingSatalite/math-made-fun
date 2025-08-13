ae334f9a846c10a7336f8a390f0cc22206af0c10
RisingSatalite <saihajmann07@gmail.com>
2025-08-12 22:46:43 -0600

    easily switch euqations

--- src/app/calculate/[equation]/page.js
+++ src/app/calculate/[equation]/page.js
@@ -1,11 +1,16 @@
 'use client'
 
+import { useState } from "react"
+
 export default function Page({ params }) {
   // params.id comes directly from the URL
 
   // const cannot be used since nerdamer gets modified when other modules are loaded  
   var nerdamer = require('nerdamer'); 
-  // Load additional modules. These are not required.  
+  // Load additional modules. These are not required.
+
+  
+  const [equation, setEquation] = useState("")
 
   var solution = nerdamer(params.equation);
   return (
@@ -13,7 +18,17 @@ export default function Page({ params }) {
       <div className="grid items-left justify-items-left">
         <h1>You entered: {params.equation}</h1>
         <p>{solution.text()}</p>
-      </div>
+        <input 
+          value={equation}
+          onChange={(e) => setEquation(e.target.value)}
+          placeholder="Enter equation"
+          className="border px-2 py-1"
+        />
+        <a href={`/calculate/${encodeURIComponent(equation)}`}>
+          Solve {equation}
+        </a>
+      </div>      
+
     </div>
   );
 }
