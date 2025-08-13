3ec4f6d9dce80eebdfd9ae0ce5b53519ca554937
RisingSatalite <saihajmann07@gmail.com>
2025-08-12 21:55:18 -0600

    fix errors

--- src/app/calculate/page.js
+++ src/app/calculate/page.js
@@ -4,10 +4,18 @@ import { useState } from "react"
 
 export default function EquationsSolver() {
     const [equation, setEquation] = useState("")
+
     return (
-        <div className="grid items-center justify-items-center">
-            <input onChange={(e) => setEquation(e.target)}></input>
-            <a href={`/calculate/`}>Solve {equation}</a>
+        <div className="grid items-center justify-items-center gap-2">
+            <input 
+                value={equation}
+                onChange={(e) => setEquation(e.target.value)}
+                placeholder="Enter equation"
+                className="border px-2 py-1"
+            />
+            <a href={`/calculate/${encodeURIComponent(equation)}`}>
+                Solve {equation}
+            </a>
         </div>
     )
 }
