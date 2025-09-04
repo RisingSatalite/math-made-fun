79aa4e839ff78c224501719cfba89d2766374f4c
RisingSatalite <saihajmann07@gmail.com>
2025-09-04 16:14:03 -0600

    create glossary and new pages

---
+++ src/app/glossary/constants/page.js
@@ -0,0 +1,12 @@
+export default function Glossary() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <h3>Constant</h3>
+      <p>A constant is any part of an equation that stays consistant as the varilbes changes and affect the rest</p>
+      <br/>
+      <h2>Examples</h2>
+      <p>5x=y</p>
+      <p>In the equation, the 5 would be a constant, becuase no matter what varible the x and y are, the 5 is always a 5</p>
+    </div>
+  );
+}
