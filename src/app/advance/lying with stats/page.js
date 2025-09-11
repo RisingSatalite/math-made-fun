d55429e0e1c02e36c771ca08c40e2b48211952f7
RisingSatalite <saihajmann07@gmail.com>
2025-09-11 11:40:23 -0600

    new page

---
+++ src/app/advance/lying with stats/page.js
@@ -0,0 +1,15 @@
+export default function __Default__() {
+    //Insert examples of lying with statictics
+    return (
+        <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+            <h1>It is very easy to lie with charts</h1>
+            <p>Save 75$ vs 75%</p>
+            <p>Save 75$, 1200 to 1125</p>
+            <p>Save 75%, 100 to 25</p>
+            <p>Show graph from 0%, from data, or full 100%, all hide data or amplfy data</p>
+            <p>From 0%, shows, does not show compared to whole</p>
+            <p>From data, does not show compared to whole, and can display ampled changes compared to whole</p>
+            <p>To 100%, can minimize trends</p>
+        </div>
+    );
+}
