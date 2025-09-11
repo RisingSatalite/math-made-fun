7728a5b905470044cd71b15e506fa4cc892b2a09
RisingSatalite <saihajmann07@gmail.com>
2025-09-11 10:44:16 -0600

    edit sampling page

--- src/app/advance/sampling/page.js
+++ src/app/advance/sampling/page.js
@@ -1,8 +1,9 @@
 export default function Sampling() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <h1>Sampling stragies</h1>
         <h2>Simple random sample</h2>
-        <p>Assigning numbers, and choosing a rnadom element, by pulling numbers</p>
+        <p>Assigning numbers, and choosing a random element, by pulling numbers</p>
         <h2>Stratified random</h2>
         <p>Divid the whole in groups, pick a random sample from every group</p>
         <h2>Cluster random sampling</h2>
