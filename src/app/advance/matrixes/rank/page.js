61d8548c013eb4438dc8c24dc5eef98b649fa6b5
RisingSatalite <saihajmann07@gmail.com>
2025-09-17 14:46:00 -0600

    martix changes

---
+++ src/app/advance/matrixes/rank/page.js
@@ -0,0 +1,9 @@
+export default function __Default__() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <p>[[1,0,3],[0,1,5],[0,0,0]] Unique solution</p>
+      <p>[[1,2,5,7],[0,1,3,11],[0,0,0,0]] Infinite solution</p>
+      <p>[[1,2,3,4],[0,0,0,5]] No solution</p>
+    </div>
+  );
+}
