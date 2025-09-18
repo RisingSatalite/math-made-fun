0c96cbdda165bb58fea30c19730fd4f691ba79a0
RisingSatalite <saihajmann07@gmail.com>
2025-09-18 12:29:06 -0600

    stat average info page

---
+++ src/app/advance/statictics/average with stats/page.js
@@ -0,0 +1,17 @@
+export default function __Default__() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+        <p>0 0 0 5 5 8 10 13 25</p>
+        <p>Mean(Average, but staticist do notlike average.)</p>
+        <h6>Do not use average with a staticist, they will be mean with you</h6>
+        <p>Sum/total = 7.3</p>
+
+        <p>Median, the middle number 5</p>
+
+        <p>Mode, the number most common 0 becuase there are 3 0s</p>
+        <p>Mode can have no model, if no repetitions</p>
+        <p>Single model, for only 1 single highest value repetition</p>
+        <p>Multiply model, incase there are multiple value with the same highest count</p>
+    </div>
+  );
+}
