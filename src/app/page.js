5c133082dad638822984ff693fa1887e1556e91a
RisingSatalite <saihajmann07@gmail.com>
2025-09-24 13:46:03 -0600

    beginner landing page

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,10 @@ export default function Home() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       Math made fun
+
+      <p>I made this website to display and assistance with learning Math</p>
+
+      <div>View all pages here<a href={`/sitemap`}>Sitemap</a></div>
     </div>
   );
 }
