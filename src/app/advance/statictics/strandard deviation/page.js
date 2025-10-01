314c41150c86dfe70faf407b2c8ba130063bfc5d
RisingSatalite <saihajmann07@gmail.com>
2025-10-01 14:22:49 -0600

    new page

---
+++ src/app/advance/statictics/strandard deviation/page.js
@@ -0,0 +1,12 @@
+export default function __Default__() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <p>Strandard deviation σ is calcualted with</p>
+      <p>Varinace σ^2 is SD sqr or (x-µ)2/(n-1)</p>
+      <p>SD is = sqrt((x-µ)2/(n-1))</p>
+
+      <p>Sample Varinace is SD sqr or (x-x̄)2/(n-1)</p>
+      <p>Sample is = sqrt((x-x̄)2/(n-1))</p>
+    </div>
+  );
+}
