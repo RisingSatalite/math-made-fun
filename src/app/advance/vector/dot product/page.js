ae380006defd203c1a148520c75a3bc3de890df7
RisingSatalite <saihajmann07@gmail.com>
2025-10-22 15:41:14 -0600

    dot product

--- src/app/advance/vector/dot product/page.js
+++ src/app/advance/vector/dot product/page.js
@@ -1,7 +1,11 @@
 export default function DotProduct() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <p></p>
+      <p>If you have 2 vectors of the same size, you cna multiple both of vector components and then add them together to get the dot product</p>
+      <p>Ex, [1,2,3],[3,-2,4]</p>
+      <p>1*3, 2*-2 and 3*4</p>
+      <p>3,-4,12</p>
+      <p>11</p>
     </div>
   );
 }
