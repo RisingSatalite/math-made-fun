83a361ef8fa9b6baca0b63643ec15ba5688a61d0
RisingSatalite <saihajmann07@gmail.com>
2025-11-19 15:15:26 -0700

    factoring example

---
+++ src/app/grade10/Factoring Polynomials/page.js
@@ -0,0 +1,9 @@
+export default function FactoringPolynomials() {
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <p>Factoring polynomials can look diffuclt, but after facotring, it can be reveal that are mostly simple smaller equations combined.</p>
+      <p>Simplify first, if all terms can be divided by something, such a number or vairble, you should so that it is simpler when you need to factor</p>
+      <p>Exmaple, you can pull a 2 out of 2x^2+4x+16 to get 2*(x^2+2x+8)</p>
+    </div>
+  );
+}
