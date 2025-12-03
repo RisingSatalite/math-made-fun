6a9e52237928f1667d4389f7754894b891870b5a
RisingSatalite <saihajmann07@gmail.com>
2025-12-03 16:31:03 -0700

    other links

--- src/app/advance/geometryTransformations/page.js
+++ src/app/advance/geometryTransformations/page.js
@@ -6,6 +6,9 @@ export default function matrixTransformations() {
       <h1>transformations</h1>
       <a href={`/advance/geometryTransformations/reflections`}>Reflections</a>
       <a href={`/advance/geometryTransformations/rotations`}>Rotations</a>
+      <a href={`/advance/geometryTransformations/projection`}>Projections</a>
+      <a href={`/advance/geometryTransformations/shear`}>Shear</a>
+      <a href={`/advance/geometryTransformations/scaling`}>Scaling</a>
       <p>When combining transformations, make sure to use A<sub>ii</sub> * A<sub>i</sub> to follow the inducing rule</p>
     </div>
   );
