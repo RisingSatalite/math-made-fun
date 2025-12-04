a3a181b5c201939ea3160649351abe93c20113ec
RisingSatalite <saihajmann07@gmail.com>
2025-12-04 10:18:40 -0700

    remove default css

--- src/components/__default__.js
+++ src/components/__default__.js
@@ -1,6 +1,6 @@
 export default function __Default__() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
+    <div className="grid items-center justify-items-center min-h-screen">
       <p></p>
     </div>
   );
