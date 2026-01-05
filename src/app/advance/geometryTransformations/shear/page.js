9ac3919ccdc4473639c9cfd3fb17d911cba48c0c
RisingSatalite <saihajmann07@gmail.com>
2026-01-04 22:05:35 -0700

    text change

--- src/app/advance/geometryTransformations/shear/page.js
+++ src/app/advance/geometryTransformations/shear/page.js
@@ -4,13 +4,13 @@ export default function Shear() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen">
       <p>Shear</p>
-      <p>To shear a matrix, we 1 of 2 formula</p>
+      <p>To shear a matrix, we will use 1 of 2 formula</p>
       Eithor
       <MatrixDisplayer matrix={[[1,"k"],[0,1]]}/>
       or
       <MatrixDisplayer matrix={[[1,0],["k",1]]}/>
       <p>k is the shear factor</p>
-      <p>We use the formula depending if we want to shear in the x factor or the y direction</p>
+      <p>The formula we use the formula depends if we want to shear in the x or the y direction</p>
     </div>
   );
 }
