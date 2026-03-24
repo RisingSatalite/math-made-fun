aeb4f234ab21306e8c261a8b3fd0d5d53d271c93
Saihaj <saihajmann07@gmail.com>
2026-03-24 09:36:08 -0600

    font change

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,6 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center p-8 pb-20 gap-16 sm:p-20">
       Math made fun
 
       <p>I made this website to display and assistance with learning Math</p>
