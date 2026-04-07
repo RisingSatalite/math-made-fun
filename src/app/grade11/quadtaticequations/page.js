b1163d3185c4e9e504f977af2dda89d86945760b
Saihaj <saihajmann07@gmail.com>
2026-04-06 19:31:50 -0600

    better QE

--- src/app/grade11/quadtaticequations/page.js
+++ src/app/grade11/quadtaticequations/page.js
@@ -1,7 +1,18 @@
 export default function QuadraticEquations() {
     return (
         <div className="grid items-center justify-items-center">
-            <p>Grade 11</p>
+            <h1>Quadratic Equations</h1>
+            <p>Quadratic equations are a type of equation that form an arch when graphed. The arc can be facing up or down.</p>
+            <p>There are 3 main way to write quadratic equations.</p>
+            <a href="/grade11/quadtaticequations/general" className="block text-indigo-400 hover:text-indigo-200 font-semibold">
+              → General Form
+            </a>
+            <a href="/grade11/quadtaticequations/factorform" className="block text-indigo-400 hover:text-indigo-200 font-semibold">
+              → Factor Form
+            </a>
+            <a href="/grade11/quadtaticequations/vertexform" className="block text-indigo-400 hover:text-indigo-200 font-semibold">
+              → Vertex Form
+            </a>
         </div>
     );
 }
