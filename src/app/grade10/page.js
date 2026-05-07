f2a05e7ffbad5832e5d4e50607065b26a3164656
Saihaj <saihajmann07@gmail.com>
2026-05-06 22:36:09 -0600

    changes

--- src/app/grade10/page.js
+++ src/app/grade10/page.js
@@ -1,12 +1,7 @@
-'use client'
-
-export default function Home() {
-
+export default function Grade10() {
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      Math made fun
-
-      Grade 10 math includes
+      <h1>Grade 10 Math</h1>
       <ul>
         <li><Link href="/grade10/linearEquations" className="text-blue-600 underline">Linear Equations — interactive</Link></li>
         <li>Algebra</li>
